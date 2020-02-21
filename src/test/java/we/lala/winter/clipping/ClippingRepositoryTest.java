86e53356ee0c390554b9fa74c0bd19bcb408dadf
ryan <whuk84@gmail.com>
2020-02-21 20:06:05 +0900

    create clipping repository save test

--- src/test/java/we/lala/winter/clipping/ClippingRepositoryTest.java
+++ src/test/java/we/lala/winter/clipping/ClippingRepositoryTest.java
@@ -1,9 +1,15 @@
 package we.lala.winter.clipping;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import we.lala.winter.domain.Clipping;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,8 +25,23 @@ class ClippingRepositoryTest {
     }
 
     @Test
+    @DisplayName("Clipping Repository 기본 저장 테스트")
     void givenClipping_whenSaveClipping_thenSaveClipping() {
+        // Given
+        Date now = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
+        Clipping clipping = Clipping.builder()
+                .textMessage("textMessage")
+                .checked(true)
+                .createDt(now)
+                .modifiedDt(now)
+                .clippedUrl("http://naver.com")
+                .numbering(1)
+                .build();
 
+        // When
+        Clipping savedClipping = clippingRepository.save(clipping);
 
+        // Then
+        assertNotNull(savedClipping);
     }
 }
