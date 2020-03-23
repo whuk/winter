5da160c98d05a2aa0a03e5e462aa3bb761b4465f
ryan <whuk84@gmail.com>
2020-03-23 21:17:55 +0900

    remove user details service

--- src/main/java/we/lala/winter/account/AccountService.java
+++ src/main/java/we/lala/winter/account/AccountService.java
@@ -1,32 +1,14 @@
 package we.lala.winter.account;
 
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import we.lala.winter.domain.Account;
 
 @Service
-public class AccountService implements UserDetailsService {
+public class AccountService {
 
     private final AccountRepository accountRepository;
-
     public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Account account = accountRepository.findByUsername(username);
-        if (account == null) {
-            throw new UsernameNotFoundException(username);
-        }
-        return User.builder()
-                .username(account.getUsername())
-                .password(account.getPassword())
-                .roles(account.getRole())
-                .build();
     }
 
     public Account createNew(Account account) {
