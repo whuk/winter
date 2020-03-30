9121535358d6482751e1ee1df41bcc98f073198e
ryan <whuk84@gmail.com>
2020-03-30 22:29:26 +0900

    remove favicon

--- src/main/java/we/lala/winter/config/SecurityConfig.java
+++ src/main/java/we/lala/winter/config/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http.authorizeExchange()
-                .pathMatchers("/favicon.ico").permitAll()
                 .pathMatchers("/", "/info", "/account/**").permitAll()
                 .pathMatchers("/admin").hasRole("ADMIN")
                 .pathMatchers("/user")
